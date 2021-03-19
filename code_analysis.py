d58a069541c4325c302de3852cb4f1287617903f
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-03-19 16:50:42 +0300

    Update code_analysis.py

--- code_analysis.py
+++ code_analysis.py
@@ -1,7 +1,7 @@
 import re,colors
 class search():
     def command_injection(line,line_number):
-        regex = "system\(.*\)|passthru\(.*\)|escapeshellcmd\(.*\)|pcntl_exec\(.*\)|exec\(.*\)|eval\(.*\)"
+        regex = "system\(.*\)|passthru\(.*\)|escapeshellcmd\(.*\)|pcntl_exec\(.*\)|exec\(.*\)|eval\(.*\)|assert\(.*\)"
         black_list = re.findall(regex, line)
         if(black_list):
             print(colors.color.red(f'found execute functions on line: {line_number+1}'))   
@@ -13,7 +13,7 @@ class search():
             print(colors.color.red(f'found LFI on line {line_number+1}'))   
             colors.color.reset()
     def XSS(line,line_number):
-        regex = "echo \$.*|echo \$_[A-Z]{2,6}\[.*\]"
+        regex = "echo \$.*|echo \$_[A-Z]{2,6}\[.*\]|echo \$\_SERVER\[\'PHP_SELF\'\]"
         black_list = re.findall(regex, line)
         if(black_list):
             print(colors.color.red(f'found XSS on line {line_number+1}'))   
@@ -32,6 +32,37 @@ class search():
         if(black_list):
             print(colors.color.red(f'found open redirect on line {line_number+1}'))   
             colors.color.reset()
+    def ID(line,line_number):
+
+        regex = "unserialize\(\.*\) |__wakeup\(\.*\)|__destruct\(\.*\)|__toString\(\.*\)"
+        black_list = re.findall(regex, line)
+        if(black_list):
+            print(colors.color.red(f'found Insecure Deserialization on line {line_number+1}'))   
+            colors.color.reset()
+    def SQLi(file):
+        regex = "query\(.*\)|mysql_query\(.*\)|get_results\(.*\)|get_var\(.*\)"
+        black_list = re.findall(regex, file)
+        if(black_list):
+            for vuln in black_list:
+                print(colors.color.red(f'found SQLi :{vuln}'))   
+                colors.color.reset()
+            
 
 
+class info():
+    def GET_parameters(file):
+        regex = "\$_GET\[.*\]"
+        black_list = re.findall(regex, file)
+        if(black_list):
+            for vuln in black_list:
+                print(colors.color.green(f'GET parameter :{vuln[7:-2]}'))   
+                colors.color.reset()
+    def POST_parameters(file):
+        regex = "\$_POST\[.*\]"
+        black_list = re.findall(regex, file)
+        if(black_list):
+            for vuln in black_list:
+                print(colors.color.green(f'POST parameter :{vuln[8:-2]}'))   
+                colors.color.reset()
+            
 
