83c0f9f0959b82065ccb4de300cd0e9cf77621de
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-03-19 16:51:03 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,28 @@
 #import libraries
 import sys
 import code_analysis as CA
-
+import colors
 #read file
-try:
+def read_all_file():
     file = open(sys.argv[1],'r',encoding="utf8")
-    line_number = 0
+    file = file.read()
+    return file 
+#information about php code 
+CA.info.GET_parameters(read_all_file())
+CA.info.POST_parameters(read_all_file())
+#scan SQL injection 
 
-    for line in file:
-        CA.search.command_injection(line.strip('\n'),line_number)
-        CA.search.LFI(line.strip('\n'),line_number)
-        CA.search.XSS(line.strip('\n'),line_number)
-        CA.search.SSRF(line.strip('\n'),line_number)
-        CA.search.open_redirect(line.strip('\n'),line_number)
-        line_number = line_number + 1
-except:
-    print(f'[+] usage: python {sys.argv[0]} file.php [+]')
+
+file = open(sys.argv[1],'r',encoding="utf8")
+line_number = 0
+print(colors.color.blue('vulnerability found:\n'))
+CA.search.SQLi(read_all_file())
+for line in file:
+    CA.search.command_injection(line.strip('\n'),line_number)
+    CA.search.LFI(line.strip('\n'),line_number)
+    CA.search.XSS(line.strip('\n'),line_number)
+    CA.search.SSRF(line.strip('\n'),line_number)
+    CA.search.open_redirect(line.strip('\n'),line_number)
+    CA.search.ID(line.strip('\n'),line_number)
+    
+    line_number = line_number + 1
