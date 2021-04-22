4a540ada65de8ee6996765b57b71ff799319265b
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-04-23 02:51:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,28 +1,42 @@
-#import libraries
-import sys
+from os import listdir
 import code_analysis as CA
-import colors
-#read file
-def read_all_file():
-    file = open(sys.argv[1],'r',encoding="utf8")
-    file = file.read()
-    return file 
-#information about php code 
-CA.info.GET_parameters(read_all_file())
-CA.info.POST_parameters(read_all_file())
-#scan SQL injection 
-
-
-file = open(sys.argv[1],'r',encoding="utf8")
-line_number = 0
-print(colors.color.blue('vulnerability found:\n'))
-CA.search.SQLi(read_all_file())
-for line in file:
-    CA.search.command_injection(line.strip('\n'),line_number)
-    CA.search.LFI(line.strip('\n'),line_number)
-    CA.search.XSS(line.strip('\n'),line_number)
-    CA.search.SSRF(line.strip('\n'),line_number)
-    CA.search.open_redirect(line.strip('\n'),line_number)
-    CA.search.ID(line.strip('\n'),line_number)
+from sys import argv
+def scan_files_in_folder(path):
     
-    line_number = line_number + 1
+    
+    folders = listdir(path)
+    for i2 in folders:
+        if('.php' in i2):        
+            
+            file = open(f"{path}/{i2}","r",encoding="utf8",errors='ignore')
+            file = file.read()
+            line_number = 0
+            print(f"{path}/{i2}")
+            CA.info.GET_parameters(file)
+            CA.info.POST_parameters(file)
+            CA.check.check_all(f"{path}/{i2}")
+            CA.search.SQLi(file)
+            CA.search.check_file_upload(file)
+            
+    for i in folders:
+        if ("." not in i):
+            scan_files_in_folder(f"{path}/{i}")
+    return folders
+    
+if(len(argv) == 2):
+    if(".php" in argv[1]):
+        file = open(f"{argv[1]}","r",encoding="utf8",errors='ignore')
+        file = file.read()
+        line_number = 0
+        CA.info.GET_parameters(file)
+        CA.info.POST_parameters(file)
+        CA.check.check_all(f"{argv[1]}")
+        CA.search.SQLi(file)
+        CA.search.check_file_upload(file)
+        CA.info.finish()
+    else:
+        scan_files_in_folder(argv[1])
+        CA.info.finish()
+else:
+    folders = scan_files_in_folder('.')
+    CA.info.finish()
