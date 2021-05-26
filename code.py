b4908874b454c2ff658df5494b703b0eb1c6afb1
mondjunge <48616831+mondjunge@users.noreply.github.com>
2021-05-26 15:09:19 +0200

    added check if path is actually a folder
    fixes problems with files that have no suffix, like commonly used README or LICENSE files in 3rd-party libs.

--- code.py
+++ code.py
@@ -1,27 +1,28 @@
 from os import listdir
 import code_analysis as CA
 from sys import argv
+import os
 def scan_files_in_folder(path):
     
-    
-    folders = listdir(path)
-    for i2 in folders:
-        if('.php' in i2):        
-            
-            file = open(f"{path}/{i2}","r",encoding="utf8",errors='ignore')
-            file = file.read()
-            line_number = 0
-            print(f"{path}/{i2}")
-            CA.info.GET_parameters(file)
-            CA.info.POST_parameters(file)
-            CA.check.check_all(f"{path}/{i2}")
-            CA.search.SQLi(file)
-            CA.search.check_file_upload(file)
-            
-    for i in folders:
-        if ("." not in i):
-            scan_files_in_folder(f"{path}/{i}")
-    return folders
+    if(os.path.isdir(path)):
+       folders = listdir(path)
+       for i2 in folders:
+           if('.php' in i2):        
+               
+               file = open(f"{path}/{i2}","r",encoding="utf8",errors='ignore')
+               file = file.read()
+               line_number = 0
+               print(f"{path}/{i2}")
+               CA.info.GET_parameters(file)
+               CA.info.POST_parameters(file)
+               CA.check.check_all(f"{path}/{i2}")
+               CA.search.SQLi(file)
+               CA.search.check_file_upload(file)
+               
+       for i in folders:
+           if ("." not in i):
+               scan_files_in_folder(f"{path}/{i}")
+       return folders
     
 if(len(argv) == 2):
     if(".php" in argv[1]):
